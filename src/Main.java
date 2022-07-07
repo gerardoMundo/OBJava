a270ad390ab5ab2e0c7feb9f528b6935cebfd653
Gerardo <ger.mundo66@gmail.com>
2022-07-06 20:41:53 -0500

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        summary(20, 30, 60);
+    }
+    public static void summary(int a, int b, int c) {
+       System.out.println(a + b + c);
 
+       Coche miCoche = new Coche();
+       miCoche.incrementarPuertas();
+       System.out.println(miCoche.incrementarPuertas());
+    }
+
+    public static class Coche {
+        public int puertas = 4;
+
+
+        public int incrementarPuertas() {
+            return puertas++;
+        }
     }
 }
